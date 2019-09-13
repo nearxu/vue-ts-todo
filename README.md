2b9edf78548f760d935db98e556356cde8e4e3f5
nearxu <2448895924@qq.com>
2019-09-13 13:13:40 +0800

    lose todo

--- README.md
+++ README.md
@@ -27,3 +27,9 @@ yarn run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+### some config eslint pretter
+
+vscode setting from 
+https://juejin.im/post/5d18e6bce51d455a694f9577
---
+++ src/components/prop-component.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>{{ count }}</div>
+  <!-- <button @click="innerCount +=1">add count</button> -->
+</template>
+<script lang="ts">
+import Vue from 'vue';
+import { Prop, Component, PropSync } from 'vue-property-decorator';
+@Component
+export default class PropComponent extends Vue {
+  @Prop({
+    type: Number,
+    validator: val => val > 100,
+    required: true,
+  })
+  private count!: string;
+
+  // @PropSync('count') private innerCount!: number;
+}
+</script>
